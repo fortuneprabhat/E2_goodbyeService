501724c31ac7006b38d67ce5106559f3b2796d62
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 12:45:14 +0530

    service 2 goodbye service

---
+++ src/main/java/com/goodbyeService/E2_goodbyeService/E2GoodbyeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goodbyeService.E2_goodbyeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2GoodbyeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2GoodbyeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodbyeService/E2_goodbyeService/E2GoodbyeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodbyeService.E2_goodbyeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2GoodbyeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
