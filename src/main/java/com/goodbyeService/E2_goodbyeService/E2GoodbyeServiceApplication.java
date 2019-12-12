db04ff124c0e7fa5ac4343038169519ae24f2ee8
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 14:18:16 +0530

    enable service as discovery client and exposing API for good bye service.

--- src/main/java/com/goodbyeService/E2_goodbyeService/E2GoodbyeServiceApplication.java
+++ src/main/java/com/goodbyeService/E2_goodbyeService/E2GoodbyeServiceApplication.java
@@ -2,12 +2,22 @@ package com.goodbyeService.E2_goodbyeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class E2GoodbyeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(E2GoodbyeServiceApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String goodBye() {
+		return "Goodbye Service";
 	}
 
 }
